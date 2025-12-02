b662e82f39001468f4df0c04e2f907995a30a101
xstefank <xstefank122@gmail.com>
2025-12-02 12:56:24 +0100

    Fix NarayanaLRAClient coordinator URL priority first by constructor then by property
    Signed-off-by: xstefank <xstefank122@gmail.com>

--- arquillian-extension/src/main/java/io/narayana/lra/arquillian/appender/MpLraTckAuxiliaryArchiveAppender.java
+++ arquillian-extension/src/main/java/io/narayana/lra/arquillian/appender/MpLraTckAuxiliaryArchiveAppender.java
@@ -81,9 +81,6 @@ public class MpLraTckAuxiliaryArchiveAppender implements AuxiliaryArchiveAppende
                         "beans.xml")
                 // for WildFly we need dependencies to be part of the deployment's class path
                 .addAsManifestResource(new StringAsset(ManifestMF), "MANIFEST.MF");
-        // add MP config properties file
-        archive.addAsResource(new StringAsset("lra.coordinator.urls=http://localhost:50000, http://localhost:50001"),
-                "META-INF/microprofile-config.properties");
         archive.addPackages(true, io.narayana.lra.filter.ClientLRARequestFilter.class.getPackage())
                 .addAsResource(new StringAsset("org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder"),
                         "META-INF/services/jakarta.ws.rs.client.ClientBuilder");
